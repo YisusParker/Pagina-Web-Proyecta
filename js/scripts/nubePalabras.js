bd2f1d1338c3e1a1d7ca72994ec09f0caee8265a
Jesús David Arévalo Montilla <62314146+YisusParker@users.noreply.github.com>
2024-11-06 17:04:15 -0500

    Added changes on index
    -cloud of words
    -header
    -file for testing
    -readme
    -script for the word cloud

---
+++ js/scripts/nubePalabras.js
@@ -0,0 +1,25 @@
+// Datos de palabras y sus frecuencias
+const words = [
+  ["COLABOREMOS PARA", 12],
+  ["MEJORAR EL BIENESTAR DE TU EQUIPO", 3],
+  ["FORTALECER HERRAMIENTAS DE CRECIMIENTO GRUPAL", 4],
+  ["POTENCIAR EL DESARROLLO EMOCIONAL DE TU EQUIPO", 4]
+];
+
+// Configuración de la nube de palabras
+function generarNubeDePalabras() {
+  WordCloud(document.getElementById('wordCloudCanvas'), {
+    list: words,
+    gridSize: 12,
+    weightFactor: 4,
+    fontFamily: 'Arial',
+    color: '#F06335',
+    rotateRatio: 0,
+    backgroundColor: '#f2f2f2'
+  });
+}
+
+// Llama a la función para crear la nube de palabras
+document.addEventListener("DOMContentLoaded", generarNubeDePalabras);
+
+  
